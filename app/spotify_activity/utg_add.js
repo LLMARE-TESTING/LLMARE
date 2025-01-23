98799507242cc7aaf0c0e3b260a4ad8fdda6c979
LLMARE-TESTING <jran@hust.edu.cn>
2025-01-23 09:54:20 +0800

    app-spotify

---
+++ app/spotify_activity/utg_add.js
@@ -0,0 +1,342 @@
+var utg = 
+{
+    "nodes": [
+        {
+            "image": "state1.png",
+            "activity": "MainActivity",
+            "state_str": "state1"
+        },
+        {
+            "image": "state2.png",
+            "activity": "MainActivity",
+            "state_str": "state2"
+        },
+        {
+            "image": "state3.png",
+            "activity": "MainActivity",
+            "state_str": "state3"
+        },
+        {
+            "image": "state4.png",
+            "activity": "MainActivity",
+            "state_str": "state4"
+        },
+        {
+            "image": "state5.png",
+            "activity": "MainActivity",
+            "state_str": "state5"
+        },
+        {
+            "image": "state6.png",
+            "activity": "MainActivity",
+            "state_str": "state6"
+        },
+        {
+            "image": "state7.png",
+            "activity": "com.spotify.profile.editprofile.editprofile.EditProfileActivity",
+            "state_str": "state7"
+        },
+        {
+            "image": "state8.png",
+            "activity": "com.spotify.profile.editprofile.editprofile.EditProfileActivity",
+            "state_str": "state8"
+        },
+        {
+            "image": "state9.png",
+            "activity": "MainActivity",
+            "state_str": "state9"
+        },
+        {
+            "image": "state10.png",
+            "activity": "MainActivity",
+            "state_str": "state10"
+        },
+        {
+            "image": "state11.png",
+            "activity": "com.spotify.tome.pageactivity.PageActivity",
+            "state_str": "state11"
+        },
+        {
+            "image": "state12.png",
+            "activity": "com.spotify.tome.pageactivity.PageActivity",
+            "state_str": "state12"
+        },
+        {
+            "image": "state13.png",
+            "activity": "MainActivity",
+            "state_str": "state13"
+        },
+        {
+            "image": "state14.png",
+            "activity": "MainActivity",
+            "state_str": "state14"
+        },
+        {
+            "image": "state15.png",
+            "activity": "MainActivity",
+            "state_str": "state15"
+        },
+        {
+            "image": "state16.png",
+            "activity": "MainActivity",
+            "state_str": "state16"
+        },
+        {
+            "image": "state17.png",
+            "activity": "MainActivity",
+            "state_str": "state17"
+        },
+        {
+            "image": "state18.png",
+            "activity": "MainActivity",
+            "state_str": "state18"
+        },
+        {
+            "image": "state19.png",
+            "activity": "com.spotify.nowplaying.musicinstallation.NowPlayingActivity",
+            "state_str": "state19"
+        },
+        {
+            "image": "state20.png",
+            "activity": "MainActivity",
+            "state_str": "state20"
+        },
+        {
+            "image": "state21.png",
+            "activity": "MainActivity",
+            "state_str": "state21"
+        },
+        {
+            "image": "state22.png",
+            "activity": "MainActivity",
+            "state_str": "state22"
+        },
+        {
+            "image": "state23.png",
+            "activity": "MainActivity",
+            "state_str": "state23"
+        },
+        {
+            "image": "state24.png",
+            "activity": "MainActivity",
+            "state_str": "state24"
+        },
+        {
+            "image": "state25.png",
+            "activity": "MainActivity",
+            "state_str": "state25"
+        },
+        {
+            "image": "state26.png",
+            "activity": "MainActivity",
+            "state_str": "state26"
+        },
+        {
+            "image": "state27.png",
+            "activity": "MainActivity",
+            "state_str": "state27"
+        },
+        {
+            "image": "state28.png",
+            "activity": "MainActivity",
+            "state_str": "state28"
+        },
+        {
+            "image": "state28.png",
+            "activity": "MainActivity",
+            "state_str": "state28"
+        },
+        {
+            "image": "state29.png",
+            "activity": "MainActivity",
+            "state_str": "state29"
+        },
+        {
+            "image": "state30.png",
+            "activity": "org.lineageos.audiofx.activity.ActivityMusic",
+            "state_str": "state30"
+        },
+        {
+            "image": "state31.png",
+            "activity": "MainActivity",
+            "state_str": "state31"
+        },
+        {
+            "image": "state32.png",
+            "activity": "MainActivity",
+            "state_str": "state32"
+        },
+        {
+            "image": "state33.png",
+            "activity": "com.spotify.nowplaying.musicinstallation.NowPlayingActivity",
+            "state_str": "state33"
+        }
+    ],
+    "edges": [
+        {
+            "from": "a17e568448adcdbafe37f9f618f3f5ca",
+            "to": "state1",
+            "event_str": "click(android.widget.TextView-Podcasts:[527,190][760,284])",
+        },
+        {
+            "from": "state1",
+            "to": "state2",
+            "event_str": "click(android.widget.Button-Play #29 2024年4月30日の電電電話:[882,1282][1014,1414])",
+        },
+        {
+            "from": "8525a40443e9f8deb66355fa4f61e1b5",
+            "to": "state3",
+            "event_str": "input(android.widget.EditText-com.spotify.music:id/query:[132,83][948,237])(Lover)",
+        },
+        {
+            "from": "state3",
+            "to": "state4",
+            "event_str": "click(android.widget.TextView-lover taylor swift:[209,317][948,378])",
+        },
+        {
+            "from": "a17e568448adcdbafe37f9f618f3f5ca",
+            "to": "state5",
+            "event_str": "click(android.widget.ImageView-com.spotify.music:id/faceheader_image:[22,171][154,303])",
+        },
+        {
+            "from": "state5",
+            "to": "state6",
+            "event_str": "click(android.widget.ImageView-com.spotify.music:id/profile_row_sidedrawer_image:[44,127][176,259])",
+        },
+        {
+            "from": "state6",
+            "to": "state7",
+            "event_str": "click(android.widget.Button-Edit:[44,589][199,727])",
+        },
+        {
+            "from": "state7",
+            "to": "state8",
+            "event_str": "click(android.widget.ImageView-com.spotify.music:id/edit_image:[359,303][722,666])",
+        },
+        {
+            "from": "a17e568448adcdbafe37f9f618f3f5ca",
+            "to": "state9",
+            "event_str": "click(android.widget.ImageView-com.spotify.music:id/image:[44,1242][476,1674])",
+        },
+        {
+            "from": "state9",
+            "to": "state10",
+            "event_str": "click(android.widget.ImageButton-More options for song 摯友:[948,1264][1080,1396])",
+        },
+        {
+            "from": "state10",
+            "to": "state11",
+            "event_str": "click(android.widget.TextView-Add to playlist:[154,1327][1036,1388])",
+        },
+        {
+            "from": "state11",
+            "to": "state12",
+            "event_str": "click(androidx.cardview.widget.CardView-My playlist #3:[253,824][540,885])",
+        },
+        {
+            "from": "be366c2134cc86078896f7869deedcbd",
+            "to": "state13",
+            "event_str": "click(android.view.ViewGroup-Liked Songs, Playlist • 19 songs, Pinned, :[0,598][1080,774])",
+        },
+        {
+            "from": "state13",
+            "to": "state14",
+            "event_str": "long-click(android.widget.ImageButton-android.view.View:[0,817][1080,993])",
+        },
+        {
+            "from": "state14",
+            "to": "state15",
+            "event_str": "click(android.widget.TextView-Remove from this playlist:[154,1441][1036,1502])",
+        },
+        {
+            "from": "state2",
+            "to": "state16",
+            "event_str": "click(android.widget.ImageView-com.spotify.music:id/artwork:[370,593][710,933])",
+        },
+        {
+            "from": "state16",
+            "to": "state17",
+            "event_str": "click(android.widget.TextView-com.spotify.music:id/title:[209,553][1036,675])",
+        },
+        {
+            "from": "state17",
+            "to": "state18",
+            "event_str": "scroll-up(android.view.ViewGroup-com.spotify.music:id/section_header:[44,1697][1036,1772])",
+        },
+        {
+            "from": "40e3d8c4e5e1c9c5d8d5ec60f3bc145e",
+            "to": "state19",
+            "event_str": "click(android.view.ViewGroup-com.spotify.music:id/lyrics_widget_container:[44,2060][1036,2148])",
+        },
+        {
+            "from": "40e3d8c4e5e1c9c5d8d5ec60f3bc145e",
+            "to": "state20",
+            "event_str": "long-click(android.view.ViewGroup-My playlist #3, Playlist • ran Jin, :[948,1264][1080,1396])",
+        },
+        {
+            "from": "be366c2134cc86078896f7869deedcbd",
+            "to": "state21",
+            "event_str": "click(android.view.View-Downloaded, show only downloaded.:[754,317][1049,449])",
+        },
+        {
+            "from": "state21",
+            "to": "state22",
+            "event_str": "click(android.view.ViewGroup-Your Episodes, 4 downloaded episodes, Downloaded, Pinned, :[0,598][1080,774])",
+        },
+        {
+            "from": "state5",
+            "to": "state23",
+            "event_str": "click(TextView-Settings and privacy:[154,815][558,876])",
+        },
+        {
+            "from": "state23",
+            "to": "state24",
+            "event_str": "click(TextView-Playback:[0,589][1080,754])",
+        },
+        {
+            "from": "state24",
+            "to": "state25",
+            "event_str": "scroll-down(com.spotify.music:id/compose_view:[44,1351][309,1412])",
+        },
+        {
+            "from": "state23",
+            "to": "state26",
+            "event_str": "click(TextView-Playback:[0,589][1080,754])",
+        },
+        {
+            "from": "a17e568448adcdbafe37f9f618f3f5ca",
+            "to": "state27",
+            "event_str": "click(android.widget.LinearLayout-com.spotify.music:id/create_tab:[810,1999][1080,2148])",
+        },
+        {
+            "from": "state23",
+            "to": "state28",
+            "event_str": "click(android.view.View-Data saving and offline:[0,1249][1080,1414])",
+        },
+        {
+            "from": "state25",
+            "to": "state29",
+            "event_str": "scroll-down(com.spotify.music:id/compose_view:[44,1351][309,1412])",
+        },
+        {
+            "from": "state29",
+            "to": "state30",
+            "event_str": "click(android.widget.TextView:[0,440][1080,605])",
+        },
+        {
+            "from": "state13",
+            "to": "state31",
+            "event_str": "scroll-up(com.spotify.music:id/header_layout:[0,0][1080,2148])",
+        },
+        {
+            "from": "be366c2134cc86078896f7869deedcbd",
+            "to": "state32",
+            "event_str": "click(com.spotify.music:id/row_root:[0,818][1080,994])",
+        },
+        {
+            "from": "40e3d8c4e5e1c9c5d8d5ec60f3bc145e",
+            "to": "state33",
+            "event_str": "scroll-down(com.spotify.music:id/image:[66,257][1014,1205])",
+        }
+    ]
+}
