429f49d57996b692fa35b09c1c1b23038a4428fe
LLMARE-TESTING <jran@hust.edu.cn>
2025-04-01 18:15:55 +0800

    app/capcut

---
+++ app/capcut_activity/utg_add.js
@@ -0,0 +1,286 @@
+var utg = {
+    "nodes" : [
+        {
+            "image": "state1.png",
+            "activity": "com.vega.main.MainActivity",
+            "state_str": "state1"
+        },
+        {
+            "image": "state2.png",
+            "activity": "com.vega.main.MainActivity",
+            "state_str": "state2"
+        },
+        {
+            "image": "state3.png",
+            "activity": "com.vega.main.MainActivity",
+            "state_str": "state3"
+        },
+        {
+            "image": "state4.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state4"
+        },
+        {
+            "image": "state5.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state5"
+        },
+        {
+            "image": "state6.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state6"
+        },
+        {
+            "image": "state7.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state7"
+        },
+        {
+            "image": "state8.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state8"
+        },
+        {
+            "image": "state9.png",
+            "activity": "com.vega.gallery.activity.MediaSelectActivity",
+            "state_str": "state9"
+        },
+        {
+            "image": "state10.png",
+            "activity": "com.vega.gallery.activity.MediaSelectActivity",
+            "state_str": "state10"
+        },
+        {
+            "image": "state11.png",
+            "activity": "com.vega.gallery.activity.MediaSelectActivity",
+            "state_str": "state11"
+        },
+        {
+            "image": "state12.png",
+            "activity": "com.vega.multicutsame.view.MultiCutSamePreviewActivity",
+            "state_str": "state12"
+        },
+        {
+            "image": "state13.png",
+            "activity": "com.vega.recorder.LvRecordActivity",
+            "state_str": "state13"
+        },
+        {
+            "image": "state14.png",
+            "activity": "com.vega.recorder.LvRecordActivity",
+            "state_str": "state14"
+        },
+        {
+            "image": "state15.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state15"
+        },
+        {
+            "image": "state16.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state16"
+        },
+        {
+            "image": "state17.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state17"
+        },
+        {
+            "image": "state18.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state18"
+        },
+        {
+            "image": "state19.png",
+            "activity": "com.vega.main.MainActivity",
+            "state_str": "state19"
+        },
+        {
+            "image": "state20.png",
+            "activity": "com.vega.main.MainActivity",
+            "state_str": "state20"
+        },
+        {
+            "image": "state21.png",
+            "activity": "com.vega.main.MainActivity",
+            "state_str": "state21"
+        },
+        {
+            "image": "state22.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state22"
+        },
+        {
+            "image": "state23.png",
+            "activity": "com.vega.multicutsame.view.MultiCutSamePreviewActivity",
+            "state_str": "state23"
+        },
+        {
+            "image": "state24.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state24"
+        },
+        {
+            "image": "state25.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state25"
+        },
+        {
+            "image": "state26.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state26"
+        },
+        {
+            "image": "state27.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state27"
+        },
+        {
+            "image": "state28.png",
+            "activity": "com.vega.edit.editpage.activity.EditActivity",
+            "state_str": "state28"
+        }
+    ],
+    "edges" : [
+        {
+            "from": "b3c2b8cf7b8f3959cd3c2bd09dbce71a",
+            "to": "state1",
+            "event_str": "click(com.lemon.lvoverseas:id/home_tool_iv:[849,221][1025,375])",
+        },
+        {
+            "from": "b3c2b8cf7b8f3959cd3c2bd09dbce71a",
+            "to": "state2",
+            "event_str": "click(com.lemon.lvoverseas:id/create_project_tv:[443,639][735,707])",
+        },
+        {
+            "from": "state2",
+            "to": "state3",
+            "event_str": "click(com.lemon.lvoverseas:id/iv_local_multi_media_select_index:[290,287][339,336])",
+        },
+        {
+            "from": "state21",
+            "to": "state4",
+            "event_str": "click(com.lemon.lvoverseas:id/sb_media_select_done:[843,1823][1036,1900])",
+        },
+        {
+            "from": "state4",
+            "to": "state5",
+            "event_str": "scroll-left(com.lemon.lvoverseas:id/dock_spread_view:[0,1956][1080,2148])",
+        },
+        {
+            "from": "state5",
+            "to": "state6",
+            "event_str": "scroll-left(com.lemon.lvoverseas:id/dock_spread_view:[0,1956][1080,2148])",
+        },
+        {
+            "from": "state4",
+            "to": "state7",
+            "event_str": "click(com.lemon.lvoverseas:id/abFullscreenPreview:[28,1188][111,1271])",
+        },
+        {
+            "from": "state4",
+            "to": "state8",
+            "event_str": "click(com.lemon.lvoverseas:id/ivIcon:[63,1983][146,2066])",
+        },
+        {
+            "from": "b3c2b8cf7b8f3959cd3c2bd09dbce71a",
+            "to": "state9",
+            "event_str": "click(com.lemon.lvoverseas:id/home_tool_iv_container:[253,221][430,375])",
+        },
+        {
+            "from": "state9",
+            "to": "state10",
+            "event_str": "click(com.lemon.lvoverseas:id/iv_local_multi_media_select_index:[290,287][339,336])",
+        },
+        {
+            "from": "state10",
+            "to": "state11",
+            "event_str": "click(com.lemon.lvoverseas:id/iv_local_multi_media_select_index:[650,287][699,336])",
+        },
+        {
+            "from": "state11",
+            "to": "state12",
+            "event_str": "click(com.lemon.lvoverseas:id/sb_media_select_done:[833,2013][1036,2090])",
+        },
+        {
+            "from": "state1",
+            "to": "state13",
+            "event_str": "click(com.lemon.lvoverseas:id/home_tool_iv_container:[253,514][430,668])",
+        },
+        {
+            "from": "state13",
+            "to": "state14",
+            "event_str": "click(com.lemon.lvoverseas:id/collapse_btn:[967,735][1033,801])",
+        },
+        {
+            "from": "state8",
+            "to": "state15",
+            "event_str": "click(com.lemon.lvoverseas:id/ivPlay:[499,1188][582,1271])",
+        },
+        {
+            "from": "state8",
+            "to": "state16",
+            "event_str": "click(com.lemon.lvoverseas:id/ivIcon:[344,1983][427,2066])",
+        },
+        {
+            "from": "state16",
+            "to": "state17",
+            "event_str": "click(com.lemon.lvoverseas:id/titleTips:[556,1983][639,2066])",
+        },
+        {
+            "from": "state17",
+            "to": "state18",
+            "event_str": "click(com.lemon.lvoverseas:id/image:[415,1807][569,1961])",
+        },
+        {
+            "from": "state2",
+            "to": "state19",
+            "event_str": "click(android.widget.TextView:[744,177][875,231])",
+        },
+        {
+            "from": "state19",
+            "to": "state20",
+            "event_str": "click(com.lemon.lvoverseas:id/iv_local_multi_media_select_index:[290,287][339,336])",
+        },
+        {
+            "from": "state3",
+            "to": "state21",
+            "event_str": "click(com.lemon.lvoverseas:id/iv_local_multi_media_select_index:[650,287][699,336])",
+        },
+        {
+            "from": "state4",
+            "to": "state22",
+            "event_str": "click(com.lemon.lvoverseas:id/ivTransition:[844,1503][921,1580])",
+        },
+        {
+            "from": "a5292e22104d48fcf7635ccf536bdca3",
+            "to": "state23",
+            "event_str": "click(com.lemon.lvoverseas:id/ivExport:[842,116][1036,204])",
+        },
+        {
+            "from": "state8",
+            "to": "state24",
+            "event_str": "scroll-left(android.view.ViewGroup:[811,1956][976,2148])",
+        },
+        {
+            "from": "state24",
+            "to": "state25",
+            "event_str": "scroll-left(android.view.ViewGroup:[811,1956][976,2148])",
+        },
+        {
+            "from": "state25",
+            "to": "state26",
+            "event_str": "scroll-left(android.view.ViewGroup:[811,1956][976,2148])",
+        },
+        {
+            "from": "state26",
+            "to": "state27",
+            "event_str": "scroll-left(android.view.ViewGroup:[811,1956][976,2148])",
+        },
+        {
+            "from": "state27",
+            "to": "state28",
+            "event_str": "scroll-left(android.view.ViewGroup:[811,1956][976,2148])",
+        }
+    ]
+}
