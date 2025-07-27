0adc67d32240c63e679bbf6ec18a5a7fb2919bda
LLMARE-TESTING <jran@hust.edu.cn>
2025-07-27 16:58:54 +0800

    instagram

---
+++ app/instagram_activity/utg_add.js
@@ -0,0 +1,301 @@
+var utg = {
+    "nodes" : [
+        {
+            "image": "state1.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state1"
+        },
+        {
+            "image": "state2.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state2"
+        },
+        {
+            "image": "state3.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state3"
+        },
+        {
+            "image": "state4.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state4"
+        },
+        {
+            "image": "state5.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state5"
+        },
+        {
+            "image": "state6.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state6"
+        },
+        {
+            "image": "state7.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state7"
+        },
+        {
+            "image": "state8.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state8"
+        },
+        {
+            "image": "state9.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state9"
+        },
+        {
+            "image": "state10.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state10"
+        },
+        {
+            "image": "state11.png",
+            "activity": "ui0",
+            "state_str": "state11"
+        },
+        {
+            "image": "state12.png",
+            "activity": "com.instagram.modal.ModalActivity",
+            "state_str": "state12"
+        },
+        {
+            "image": "state13.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state13"
+        },
+        {
+            "image": "state14.png",
+            "activity": "com.instagram.modal.ModalActivity",
+            "state_str": "state14"
+        },
+        {
+            "image": "state15.png",
+            "activity": "com.instagram.modal.TransparentModalActivity",
+            "state_str": "state15"
+        },
+        {
+            "image": "state16.png",
+            "activity": "com.instagram.modal.TransparentModalActivity",
+            "state_str": "state16"
+        },
+        {
+            "image": "state17.png",
+            "activity": "com.instagram.modal.ModalActivity",
+            "state_str": "state17"
+        },
+        {
+            "image": "state18.png",
+            "activity": "com.instagram.modal.ModalActivity",
+            "state_str": "state18"
+        },
+        {
+            "image": "state19.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state19"
+        },
+        {
+            "image": "state20.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state20"
+        },
+        {
+            "image": "state21.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state21"
+        },
+        {
+            "image": "state22.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state22"
+        },
+        {
+            "image": "state23.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state23"
+        },
+        {
+            "image": "state24.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state24"
+        },
+        {
+            "image": "state25.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state25"
+        },
+        {
+            "image": "state26.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state26"
+        },
+        {
+            "image": "state27.png",
+            "activity": "com.instagram.mainactivity.InstagramMainActivity",
+            "state_str": "state27"
+        }
+    ],
+    "edges" : [
+        {
+            "from": "52e92cce957416c52f0c11fa5490fd56",
+            "to": "state1",
+            "event_str": "click(com.instagram.android:id/profile_tab-Profile:[864,2027][1080,2148])"
+        },
+        {
+            "from": "state1",
+            "to": "state2",
+            "event_str": "click(android.widget.LinearLayout-com.instagram.android:id/pog_note_bubble_root_view:[44,259][225,402])"
+        },
+        {
+            "from": "52e92cce957416c52f0c11fa5490fd56",
+            "to": "state3",
+            "event_str": "click(com.instagram.android:id/clips_tab-Reels:[648,2027][864,2148])"
+        },
+        {
+            "from": "state4",
+            "to": "state5",
+            "event_str": "scroll-down(android.view.View:[0,237][1080,2027])"
+        },
+        {
+            "from": "state5",
+            "to": "state6",
+            "event_str": "scroll-down(android.view.View:[0,237][1080,2027])"
+        },
+        {
+            "from": "state6",
+            "to": "state7",
+            "event_str": "scroll-down(android.view.View:[0,237][1080,2027])"
+        },
+        {
+            "from": "state7",
+            "to": "state8",
+            "event_str": "scroll-down(android.view.View:[0,237][1080,2027])"
+        },
+        {
+            "from": "state8",
+            "to": "state9",
+            "event_str": "scroll-down(android.view.View:[0,237][1080,2027])"
+        },
+        {
+            "from": "state9",
+            "to": "state10",
+            "event_str": "scroll-down(android.view.View:[0,237][1080,2027])"
+        },
+        {
+            "from": "state10",
+            "to": "state11",
+            "event_str": "click(android.widget.Button-Log out:[0,1781][1080,1911])"
+        },
+        {
+            "from": "state11",
+            "to": "state12",
+            "event_str": "click(com.instagram.android:id/primary_button-Log out:[237,1157][842,1289])"
+        },
+        {
+            "from": "state12",
+            "to": "52e92cce957416c52f0c11fa5490fd56",
+            "event_str": "click(android.widget.Button-Log in:[44,1361][1036,1482])"
+        },
+        {
+            "from": "fcd954acdaa10214155f6180f2a9176e",
+            "to": "state13",
+            "event_str": "click(com.instagram.android:id/background_color:[5,627][358,1254])"
+        },
+        {
+            "from": "state13",
+            "to": "state14",
+            "event_str": "click(com.instagram.android:id/clips_right_action_button:[852,2028][1047,2148])"
+        },
+        {
+            "from": "state14",
+            "to": "state3",
+            "event_str": "click(com.instagram.android:id/share_button:[562,1972][1036,2093])"
+        },
+        {
+            "from": "state1",
+            "to": "state15",
+            "event_str": "click(com.instagram.android:id/seen_state:[31,1573][237,1779])"
+        },
+        {
+            "from": "state15",
+            "to": "state16",
+            "event_str": "click(com.instagram.android:id/self_toolbar_menu_button-More options:[893,1946][1069,2119])"
+        },
+        {
+            "from": "state16",
+            "to": "state17",
+            "event_str": "click(com.instagram.android:id/action_sheet_row_text_view-Edit highlight:[0,1589][1080,1719])"
+        },
+        {
+            "from": "state17",
+            "to": "state18",
+            "event_str": "click(com.instagram.android:id/edit_cover_link-Edit cover:[454,496][625,582])"
+        },
+        {
+            "from": "640f885d25fb1fabfb773841ae34d42c",
+            "to": "state19",
+            "event_str": "click(android.view.View-Layout button:[34,1142][160,1279])"
+        },
+        {
+            "from": "state19",
+            "to": "state20",
+            "event_str": "click(com.instagram.android:id/gallery_button_view:[215,425][325,535])"
+        },
+        {
+            "from": "963b604a07910978be21d1ebdb09110a",
+            "to": "state21",
+            "event_str": "click(android.widget.Button-Shutter:[453,1738][628,1913])"
+        },
+        {
+            "from": "state21",
+            "to": "state22",
+            "event_str": "click(com.instagram.android:id/asset_button-Emojis and stickers:[529,44][650,165])"
+        },
+        {
+            "from": "state22",
+            "to": "state23",
+            "event_str": "click(android.widget.ImageView-Sticker:[303,1884][509,2090])"
+        },
+        {
+            "from": "state23",
+            "to": "52e92cce957416c52f0c11fa5490fd56",
+            "event_str": "click(android.widget.Button-Your story:[33,2002][464,2122])"
+        },
+        {
+            "from": "state3",
+            "to": "state24",
+            "event_str": "click(com.instagram.android:id/clips_ufi_more_button_component-More:[937,1763][1058,1884])"
+        },
+        {
+            "from": "state1",
+            "to": "state25",
+            "event_str": "scroll-down(com.instagram.android:id/profile_header_fixed_list:[0,237][1080,1874])"
+        },
+        {
+            "from": "state25",
+            "to": "state26",
+            "event_str": "scroll-up(com.instagram.android:id/profile_header_fixed_list:[0,237][1080,1874])"
+        },
+        {
+            "from": "state26",
+            "to": "state4",
+            "event_str": "click(android.widget.Button-Options:[926,83][1058,237])"
+        },
+        {
+            "from": "52e92cce957416c52f0c11fa5490fd56",
+            "to": "state27",
+            "event_str": "scroll-down(com.instagram.android:id/profile_header_fixed_list:[0,237][1080,1874])"
+        },
+        {
+            "from": "52e92cce957416c52f0c11fa5490fd56",
+            "to": "3cfa860cab4107ace85a8194a08407ec",
+            "event_str": "click(com.instagram.android:id/creation_tab:[432,2027][648,2148])"
+        },
+        {
+            "from": "52e92cce957416c52f0c11fa5490fd56",
+            "to": "df1ab4defbaaab708176b696141ae895",
+            "event_str": "click(com.instagram.android:id/search_tab:[216,2027][432,2148])"
+        }
+    ]
+}
