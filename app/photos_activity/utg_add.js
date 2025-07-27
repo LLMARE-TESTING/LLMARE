3b30787caf0507452f636a4b961bcb5f971ca177
LLMARE-TESTING <jran@hust.edu.cn>
2025-07-27 17:19:12 +0800

    google photos

---
+++ app/photos_activity/utg_add.js
@@ -0,0 +1,339 @@
+var utg = 
+{
+  "nodes": [
+    {
+      "image": "state1.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state1"
+    },
+    {
+        "image": "state2.png",
+        "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+        "state_str": "state2"
+    },
+    {
+        "image": "state3.png",
+        "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+        "state_str": "state3"
+    },
+    {
+        "image": "state4.png",
+        "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+        "state_str": "state4"
+    },
+    {
+      "image": "state5.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state5"
+
+    },
+    {
+      "image": "state6.png",
+      "activity": "create.CreateActivity",
+      "state_str": "state6"
+
+    },
+    {
+      "image": "state7.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state7"
+    },
+    {
+      "image": "state8.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state8"
+    },
+    {
+      "image": "state9.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state9"
+    },
+    {
+      "image": "state10.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state10"
+    },
+    {
+      "image": "state11.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state11"
+    },
+    {
+      "image": "state12.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state12"
+    },
+    {
+      "image": "state13.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state13"
+    },
+    {
+      "image": "state14.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state14"
+    },
+    {
+      "image": "state15.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state15"
+    },
+    {
+      "image": "state16.png",
+      "activity": "envelope.AlbumActivity",
+      "state_str": "state16"
+    },
+    {
+      "image": "state17.png",
+      "activity": "envelope.AlbumActivity",
+      "state_str": "state17"
+    },
+    {
+      "image": "state18.png",
+      "activity": "envelope.AlbumActivity",
+      "state_str": "state18"
+    },
+    {
+      "image": "state20.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state20"
+    },
+    {
+      "image": "state21.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state21"
+    },
+    {
+      "image": "state22.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state22"
+    },
+    {
+      "image": "state23.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state23"
+    },
+    {
+      "image": "state24.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state24"
+    },
+    {
+      "image": "state26.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state26"
+    },
+    {
+      "image": "state27.png",
+      "activity": "photoeditor.fragments.activity.VideoPhotoEditorActivity",
+      "state_str": "state27"
+    },
+    {
+      "image": "state28.png",
+      "activity": "photoeditor.fragments.activity.VideoPhotoEditorActivity",
+      "state_str": "state28"
+    },
+    {
+      "image": "state29.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state29"
+    },
+    {
+      "image": "state30.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state30"
+    },
+    {
+      "image": "state31.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state31"
+    },
+    {
+      "image": "state32.png",
+      "activity": "photoeditor.fragments.activity.PhotoEditorActivity",
+      "state_str": "state32"
+    },
+    {
+      "image": "state33.png",
+      "activity": "home.HomeActivity",
+      "state_str": "state33"
+    },
+    {
+      "image": "state34.png",
+      "activity": "collectionstab.collectionsgridpage.CollectionsGridPageActivity",
+      "state_str": "state34"
+    },
+    {
+      "image": "state35.png",
+      "activity": "backup.overview.impl.BackupOverviewActivity",
+      "state_str": "state35"
+    }
+],
+"edges": [
+    {
+        "from": "fdedbaae0d7db088bdc1a119bf22aa84",
+        "to": "state1",
+        "event_str": "click(ImageView-Photo taken on Apr 7, 2024 4:50:29 PM:[0,457][355,812])",
+      },
+    {
+      "from": "state1",
+      "to": "state2",
+      "event_str": "click(ImageView-Edit:[337,1993][491,2148])",
+    },
+    {
+        "from": "state2",
+        "to": "state3",
+        "event_str": "click(TextView-Change to tools tab:[952,1868][1080,1966])",
+    },
+    {
+        "from": "state3",
+        "to": "state4",
+        "event_str": "click(TextView-Magic eraser:[463,1551][617,1705])",
+    },
+    {
+      "from": "fdedbaae0d7db088bdc1a119bf22aa84",
+      "to": "state5",
+      "event_str": "click(Button-Memories:[270,1928][540,2148])"
+    },
+    {
+      "from": "state5",
+      "to": "state6",
+      "event_str": "click(Button-Creations:[661,105][793,237])"
+    },
+    {
+      "from": "fdedbaae0d7db088bdc1a119bf22aa84",
+      "to": "state8",
+      "event_str": "click(FrameLayout-Signed in as ran Jin jinran848@gmail.com.Backup is off.Account and settings.:[925,104][1080,237])"
+    },
+    {
+      "from": "state8",
+      "to": "state9",
+      "event_str": "click(ImageView-Expand account list:[904,298][970,364])"
+    },
+    {
+      "from": "state9",
+      "to": "state7",
+      "event_str": "click(TextView-Use without an account:[254,993][970,1054])"
+    },
+    {
+      "from": "state2",
+      "to": "state10",
+      "event_str": "click(TextView-Crop{Change to crop tab}:[740,1868][952,1966])"
+    },
+    {
+      "from": "state10",
+      "to": "state11",
+      "event_str": "click(TextView-Tools{Change to tools tab}:[646,1868][864,1966])"
+    },
+    {
+      "from": "state11",
+      "to": "state12",
+      "event_str": "click(TextView-Adjust{Change to adjust tab}:[649,1868][889,1966])"
+    },
+    {
+      "from": "state12",
+      "to": "state13",
+      "event_str": "click(TextView-Filters{Change to filters tab}:[660,1868][896,1966])"
+    },
+    {
+      "from": "state13",
+      "to": "state14",
+      "event_str": "click(TextView-Markup{Change to markup tab}:[658,1868][920,1966])"
+    },
+    {
+      "from": "state14",
+      "to": "state15",
+      "event_str": "click(TextView-More{More effects and editors}:[671,1868][887,1966])"
+    },
+    {
+      "from": "f58d07b2dc774e14e82a3c9db70eba73",
+      "to": "state16",
+      "event_str": "click(ImageView-com.google.android.apps.photos:id/spotlight_image:[0,303][1080,1653])"
+    },
+    {
+      "from": "state16",
+      "to": "state17",
+      "event_str": "click(ImageView-Photo taken on Jul 2, 2023 5:29:38 PM:[0,1955][582,2148])"
+    },
+    {
+      "from": "state17",
+      "to": "state18",
+      "event_str": "click(ImageView-More options:[964,94][1063,248])"
+    },
+    {
+      "from": "state1",
+      "to": "state20",
+      "event_str": "click(ImageView-More options:[964,94][1063,248])"
+    },
+    {
+      "from": "fdedbaae0d7db088bdc1a119bf22aa84",
+      "to": "state22",
+      "event_str": "long-click(ImageView-Photo taken on Apr 7, 2024 4:50:29 PM:[363,1010][718,1365])"
+    },
+    {
+      "from": "state22",
+      "to": "state21",
+      "event_str": "long-click(ImageView-Photo taken on Apr 7, 2024 4:50:29 PM:[726,1010][1080,1365])"
+    },
+    {
+      "from": "state21",
+      "to": "state23",
+      "event_str": "scroll-up(android.view.View-com.google.android.apps.photos:id/handle:[0,1895][1080,1906])"
+    },
+    {
+      "from": "state23",
+      "to": "state24",
+      "event_str": "click(View-com.google.android.apps.photos:id/recent_album_cover:[44,617][374,947])"
+    },
+    {
+      "from": "fdedbaae0d7db088bdc1a119bf22aa84",
+      "to": "state26",
+      "event_str": "click(Button-Grid controls menu button:[947,306][1052,411])"
+    },
+    {
+      "from": "455b9f0c7c955e9c8f56a725297d2b2e",
+      "to": "state27",
+      "event_str": "click(android.widget.ImageView-com.google.android.apps.photos:id/edit:[462,1993][616,2148])"
+    },
+    {
+      "from": "state27",
+      "to": "state28",
+      "event_str": "click(android.widget.TextView-Change to crop tab:[653,1868][865,1966])"
+    },
+    {
+      "from": "state12",
+      "to": "state29",
+      "event_str": "scroll-left(android.support.v7.widget.RecyclerView-com.google.android.apps.photos:id/photos_photoeditor_fragments_editor3_adjust_recyclerview:[0,1485][1080,1837])"
+    },
+    {
+      "from": "state29",
+      "to": "state30",
+      "event_str": "scroll-left(android.support.v7.widget.RecyclerView-com.google.android.apps.photos:id/photos_photoeditor_fragments_editor3_adjust_recyclerview:[0,1485][1080,1837])"
+    },
+    {
+      "from": "state30",
+      "to": "state31",
+      "event_str": "scroll-left(android.support.v7.widget.RecyclerView-com.google.android.apps.photos:id/photos_photoeditor_fragments_editor3_adjust_recyclerview:[0,1485][1080,1837])"
+    },
+    {
+      "from": "state31",
+      "to": "state32",
+      "event_str": "scroll-left(android.support.v7.widget.RecyclerView-com.google.android.apps.photos:id/photos_photoeditor_fragments_editor3_adjust_recyclerview:[0,1485][1080,1837])"
+    },
+    {
+      "from": "e7293860b66c73e5472ed81051e992dd",
+      "to": "state33",
+      "event_str": "scroll-down(android.view.View-com.google.android.apps.photos:[44,22][1036,1928])"
+    },
+    {
+      "from": "e7293860b66c73e5472ed81051e992dd",
+      "to": "state34",
+      "event_str": "click(android.view.View-com.google.android.apps.photos:[562,614][1036,1187])"
+    },
+    {
+      "from": "state8",
+      "to": "state35",
+      "event_str": "click(android.widget.TextView-Backup complete:[254,832][970,893])"
+    }
+]
+}
